d9fa4ee0b83d2db561b07859637bc6b4bcc5a4e7
Solomon <solomonmacharia7@gmail.com>
2019-01-09 01:12:40 +0300

    [finishes #163030824] Create question record models

---
+++ app/api/v1/models/question_record_models.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+
+class QuestionRecord():
+    """ Creates the question record model """
+    def __init__(self, *args):
+        self.all_question_records = []
+
+    def create_question(self, qstnId, createdOn, createdBy, meetupId, title, body, votes):
+        """ Adds a new question to the all_question_records list """
+        self.qstnId = qstnId
+        self.createdOn = datetime.now()
+        self.createdBy = createdBy
+        self.meetupId = meetupId
+        self.title = title
+        self.body = body
+        self.votes = votes
+        new_question = {
+            "question_id": qstnId,
+            "createdOn": createdOn,
+            "createdBy": createdBy,
+            "meetupId": meetupId,
+            "title": title,
+            "body": body,
+            "votes": votes
+
+        }
+        self.all_question_records.append(new_question)
