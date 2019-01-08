8e3a21e4874ec8006e240265dbf419def84fc9e6
Solomon <solomonmacharia7@gmail.com>
2019-01-09 00:22:03 +0300

    [feature #163030824] Create the create question record endpoint

--- app/api/v1/views/question_record_views.py
+++ app/api/v1/views/question_record_views.py
@@ -1,7 +1,22 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request, json
+from ..models.question_record_models import QuestionRecord
+from datetime import datetime
 
 v1_questions_blueprint = Blueprint('questions', __name__, url_prefix='/api/v1')
 
-@v1_questions_blueprint.route('/questions', methods=['GET'])
-def questions():
-    return jsonify(True)
+question = QuestionRecord()
+
+@v1_questions_blueprint.route('/questions', methods=['POST'])
+def post_question():
+    data = request.get_json()
+
+    qstnId = len(question.all_question_records) + 1
+    createdOn = datetime.now()
+    createdBy = data["createdBy"]
+    meetupId = data["meetupId"]
+    title = data["title"]
+    body = data["body"]
+    votes = data["votes"]
+
+    question.create_question(qstnId, createdOn, createdBy, meetupId, title, body, votes )
+    return jsonify({"Message": "Question posted"}), 201
