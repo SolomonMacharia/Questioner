be989c683bf3af922c6d1c72d7003a08a5951943
Solomon <solomonmacharia7@gmail.com>
2019-01-09 03:43:23 +0300

    [finishes #163030225] Create the create meetup endpoint , Make the failing tests pass

---
+++ app/api/v1/views/meetup_record_views.py
@@ -0,0 +1,30 @@
+from flask import jsonify, Blueprint, request, json
+from ..models.meetup_record_models import MeetupRecord
+from datetime import datetime
+from uuid import uuid4
+
+v1_meetup_blueprint = Blueprint('meetups', __name__, url_prefix='/api/v1')
+
+meetup = MeetupRecord()
+
+@v1_meetup_blueprint.route('/meetups', methods=['POST'])
+def post_question():
+    data = request.get_json()
+
+    meetupId = len(meetup.all_meetup_records) + 1
+    createdOn = datetime.now()
+    location = data["location"]
+    images = data["images"]
+    happeningOn = data["happeningOn"]
+    tags = data["tags"]
+
+    meetup.create_meetup(meetupId, createdOn, location, images, happeningOn, tags )
+    return jsonify({
+        "status": 201,
+        "data": [{
+            "topic": "The topic",
+            "location": "The venue",
+            "happeningOn": "The meetup date.",
+            "tags": ["tag1", "tag2", "tag3"]
+        }]
+    }), 201
