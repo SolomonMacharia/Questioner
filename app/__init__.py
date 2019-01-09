c1861436fc99b13fa6f7663778d194fad7698f96
Solomon <solomonmacharia7@gmail.com>
2019-01-09 03:41:16 +0300

    [feature #163030225] Register the meetups blueprint with the application factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 import os
 from  flask import Flask
 from app.api.v1.views.question_record_views import v1_questions_blueprint
-
+from app.api.v1.views.meetup_record_views import v1_meetup_blueprint
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(v1_questions_blueprint)
+    app.register_blueprint(v1_meetup_blueprint)
     return app
