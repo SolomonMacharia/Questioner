1fb07eba3ffe4726ed1ff2dfe429b41cd2e4fd07
Solomon <solomonmacharia7@gmail.com>
2019-01-14 22:30:05 +0300

    [feature #163179905] Add user models in the models directory

---
+++ app/api/v1/models/user_models.py
@@ -0,0 +1,28 @@
+
+all_users = []
+
+class User:
+    def __init__(self):
+        self.db = all_users
+
+    def create_user(self, username, email, password, repeatPassword):
+        new_user = {
+            "username": username,
+            "email": email,
+            "password": password,
+            "repeatPassword": repeatPassword
+        }
+        if new_user:
+            self.db.append(new_user)
+        return new_user
+
+    def get_all_users(self):
+        return self.db
+
+    def get_single_user(self, username):
+        usr = [usr for usr in all_users if usr[username] == username]
+        if usr:
+            return usr
+        return None
+
+    
