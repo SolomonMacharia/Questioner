fca17e2a0a6fd5c75c24643c6c537bc0eaba4ec5
Solomon <solomonmacharia7@gmail.com>
2019-01-17 15:53:28 +0300

    [Chore #163283296] Add more tests to cover question edge cases

--- app/tests/v1/test_question_record_views.py
+++ app/tests/v1/test_question_record_views.py
@@ -29,6 +29,48 @@ class TestQuestionModels(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
         self.assertIn("The question title", str(self.question))
         self.assertIn("The question description", str(self.question))
+    
+    def test_api_cannot_post_data_without_meetupId_field(self):
+        payload = {'title': 'title', 'body': 'body'}
+        response = self.client.post('/api/v1/questions', data=json.dumps(payload), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        result = json.loads(response.data)
+        self.assertEqual(result['errors'], ["meetupId required"])
+        
+    def test_api_cannot_post_data_without_title_field(self):
+        payload = {'meetupId': 233, 'body': 'dkfnd'}
+        response = self.client.post('/api/v1/questions', data=json.dumps(payload), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        result = json.loads(response.data)
+        self.assertEqual(result['errors'], ["title required"])
+
+    def test_api_cannot_post_data_with_no_body_field(self):
+        payload = {'meetupId': 233, 'title': 'gddjdhfsj'}
+        response = self.client.post('/api/v1/questions', data=json.dumps(payload), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        result = json.loads(response.data)
+        self.assertEqual(result['errors'], ["body required"])
+
+    def test_api_cannot_post_data_with_no_meetupId_data(self):
+        payload = {'meetupId': '', 'title': 'gddjdhfsj', 'body': 'body'}
+        response = self.client.post('/api/v1/questions', data=json.dumps(payload), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        result = json.loads(response.data)
+        self.assertEqual(result['errors'], ["meetupId cannot be empty"])
+    
+    def test_api_cannot_post_data_with_no_title_data(self):
+        payload = {'meetupId': 432, 'title': ' ', 'body': 'body'}
+        response = self.client.post('/api/v1/questions', data=json.dumps(payload), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        result = json.loads(response.data)
+        self.assertEqual(result['errors'], ["title cannot be empty"])
+
+    def test_api_cannot_post_data_with_no_body_data(self):
+        payload = {'meetupId': 432, 'title': 'title', 'body': ' '}
+        response = self.client.post('/api/v1/questions', data=json.dumps(payload), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        result = json.loads(response.data)
+        self.assertEqual(result['errors'], ["body cannot be empty"])
 
     def test_api_can_get_all_questions(self):
             res = self.client.get('/api/v1/questions', data=json.dumps(self.question), content_type='application/json')
@@ -44,7 +86,7 @@ class TestQuestionModels(unittest.TestCase):
         res = self.client.post('/api/v1/questions', data=json.dumps(self.question), content_type='application/json')
         self.assertEqual(res.status_code, 201)
         res = self.client.get('/api/v1/questions/13', content_type='application/json')
-        self.assertEqual(res.status_code, 405,)
+        self.assertEqual(res.status_code, 404,)
         response_data = json.loads(res.data)
         self.assertEqual(response_data['Error'], "question 13 doesn't exist!")
 
