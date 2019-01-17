e75ff474514c4c8c4d6db674eddfec5217bc8947
Solomon <solomonmacharia7@gmail.com>
2019-01-17 16:05:44 +0300

    [Bug #163283296] Create a validator function to validate user input

---
+++ app/api/v1/utils/validators.py
@@ -0,0 +1,16 @@
+def validate(inputdata, required_fields=[]):
+    ''' This function validates all required input fields '''
+    errors = []
+    keys = list(inputdata.keys())
+
+    for field in required_fields:
+        if field not in keys:
+            errors.append('{} required'.format(field))
+        else:
+            if len(str(inputdata[field]).strip(' ')) <= 0:
+                errors.append('{} cannot be empty'.format(field))
+
+    if len(errors) > 0:
+        return errors
+    return inputdata
+    
